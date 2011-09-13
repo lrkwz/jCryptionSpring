ddbfc8db621f1ea02158b3530e867e46d162b915
Luca Orlandi <luca.orlandi@gmail.com>
2011-09-13 22:29:56 +0200

    Refactored package naming

---
+++ src/main/java/org/gitorious/jcryption/PublicKeyImpl.java
@@ -0,0 +1,15 @@
+package org.gitorious.jcryption;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@XmlRootElement
+public class PublicKeyImpl {
+	String e;
+	String n;
+	String maxdigits;
+}
